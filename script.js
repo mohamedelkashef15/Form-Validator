9effd3cb9dcea09e2357a28aa02e7bf80e695399
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2023-11-10 00:21:11 +0200

    Edit Js file

--- script.js
+++ script.js
@@ -14,22 +14,39 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-// showError
+// Show error
 const showError = function (input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
 
   const small = formControl.querySelector("small");
+  small.style.visability = "visable";
   small.innerText = message;
 };
 
-// showSuccess
+// Show Success
 const showSuccess = function (input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 };
 
-// Validate Email
+// get field Name
+const getFiledName = function (input) {
+  return `${input.id.charAt(0).toUpperCase()}${input.id.slice(1)}`;
+};
+
+// check empty input
+const checkEmpty = function (inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value === "") {
+      showError(input, `${getFiledName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+// Email validation
 const checkEmail = function (input) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -37,41 +54,38 @@ const checkEmail = function (input) {
   if (re.test(input.value)) {
     showSuccess(input);
   } else {
-    showError(input, "Email is not valid");
+    showError(input, "Email invalid");
   }
 };
 
-// true => valid email
-// not true => invalid
-
-// Check required
-const checkRequired = function (inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, `${input.id.charAt(0).toUpperCase()}${input.id.substring(1)} is requried`);
-    }
-  });
-};
-
-// Check length
+// check length
 const checkLength = function (input, min, max) {
+  console.log(input.value.length);
   if (input.value.length < min) {
-    showError(input, `${input.id.charAt(0).toUpperCase()}${input.id.substring(1)} Must be at least ${min} character`);
+    showError(input, `${getFiledName(input)} must be at least ${min} characters.`);
   } else if (input.value.length > max) {
-    showError(input, `${input.id.charAt(0).toUpperCase()}${input.id.substring(1)} Must be less than ${max} character`);
+    showError(input, `${getFiledName(input)} must be less than ${max} characters.`);
   } else {
     showSuccess(input);
   }
 };
 
-// Check email
+// Check password match
+const checkPassword = function (input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password didn't match");
+  } else {
+    showSuccess(input2);
+  }
+};
 
-// Event Listener
+// Event listener
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
+  checkEmpty([username, email, password, password2]);
+  checkEmail(email);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
-  checkEmail(email);
+  checkPassword(password, password2);
 });
